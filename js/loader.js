2711f5c09ff8dab56e2d79c4f36c5e756f375f2a
UNMUHA <92772775+IMBASTH@users.noreply.github.com>
2024-10-09 00:04:43 +0700

    Add files via upload

---
+++ js/loader.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    // Setelah semua konten selesai dimuat
+    document.getElementById('loader').style.display = 'none'; // sembunyikan loader
+    document.getElementById('content').style.display = 'block'; // tampilkan konten
+};
